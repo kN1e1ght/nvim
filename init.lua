10af161366e65e5b7616ee6dd4f4b1171949d03c
kN1e1ght <2425292033@qq.com>
2021-10-07 20:16:22 +0800

    add gitignore

--- init.lua
+++ init.lua
@@ -1,25 +1,10 @@
 vim.cmd[[
 set termguicolors
 ]]
+
 require'impatient'.enable_profile()
-vim.g.loaded_gzip = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
+require('packer_compiled')
+require('config')
 require('packer').init(
 {
     -- Move to lua dir so impatient.nvim can cache it
@@ -27,7 +12,6 @@ require('packer').init(
   }
 )
 require("plugins")
-require('packer_compiled')
 require("opt")
 
 require("mappings")
